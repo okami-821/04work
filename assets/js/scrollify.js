2cf1d27ee2e2b13fe4f8a0138c7813d45020ebea
okami-821 <91843497+okami-821@users.noreply.github.com>
2022-05-16 10:11:05 +0900

    Add files via upload

---
+++ assets/js/scrollify.js
@@ -0,0 +1,9 @@
+$.scrollify({
+    section:'.scrollify', //対象要素を指定
+    easing: 'easeInOutCirc',
+    // easing: 'easeOutExpo', // イージングを指定
+    scrollSpeed: 1100, // スクロール時の速度
+    updateHash: false,
+    touchScroll: true,
+    // standardScrollElements: '.section02,.section03', //通常スクロール要素
+});
